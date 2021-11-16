8181b2e52afec6d32b6fd5101b1dc5acf7c6c950
Pjiwm <Pim.Munne@gmail.com>
2021-11-16 16:07:59 +0100

    added middleware for processing incorrect request

--- controllers/drivers.controller.js
+++ controllers/drivers.controller.js
@@ -12,19 +12,12 @@ class DriversController {
     }
     /**
      * Creates a new driver
-     * @param {*} req - the object that gets posted
+     * @param {*} body - the body of the request
      * @param {*} res - the response we give back after we tried to add the request object
      */
-    async create({ body }, res) {
-        console.log(body)
-        console.log('1')
-        const newDriver = await driver.create(body)
-        console.log(body)
-        console.log('2')
-        // console.log(body)
+    async create({ body }, res, next) {
+        const newDriver = await driver.create(body).catch(next)
         res.send(newDriver)
-        console.log(body)
-        console.log('3')
     }
 }
 
