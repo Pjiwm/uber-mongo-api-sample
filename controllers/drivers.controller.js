97c3142bc283a11712e961ff751783969ab56093
Pjiwm <Pim.Munne@gmail.com>
2021-11-16 17:05:57 +0100

    added read functionality for api

--- controllers/drivers.controller.js
+++ controllers/drivers.controller.js
@@ -20,12 +20,39 @@ class DriversController {
         res.send(newDriver)
     }
 
+    /**
+     * Reads from database and retrieves driver
+     * @param {*} params.id - the id of the driver we want to get as a response
+     * @param {*} res - the driver with the given id
+     */
+    async get({params}, res, next) {
+        const foundDriver = await driver.findById(params.id).catch(next)
+        res.send(foundDriver)
+    }
+    /**
+     * Reads all drivers from database
+     * @param {*} res - all drivers in database
+     */
+    async getAll(req, res, next) {
+        const drivers = await driver.find().catch(next)
+        res.send(drivers)
+    }
+
+    /**
+     * Updates a driver
+     * @param {*} params.id - the id of the driver we want to update
+     * @param {*} res - the response we give back after the driver is updated
+     */
     async edit({ body, params }, res, next) {
         const editedDriver = await driver.findByIdAndUpdate({ _id: params.id }, body).catch(next)
         res.send(await driver.findById(params.id))
     }
-
-    async delete({body, params }, res, next) {
+    /**
+     * 
+     * @param {*} params.id - the id of the driver we want to delete 
+     * @param {*} res 
+     */
+    async delete({ body, params }, res, next) {
         const removedDriver = await driver.findByIdAndDelete(params.id).catch(next)
         res.status(204).send(removedDriver)
     }
--- router/routes.js
+++ router/routes.js
@@ -2,7 +2,10 @@ const driversController = require('../controllers/drivers.controller')
 
 module.exports = (app) => {
     app.get('/api', driversController.greeting)
+
     app.post('/api/drivers', driversController.create)
+    app.get('/api/drivers/:id', driversController.get)
+    app.get('/api/drivers', driversController.getAll)
     app.put('/api/drivers/:id', driversController.edit)
     app.delete('/api/drivers/:id', driversController.delete)
 }
